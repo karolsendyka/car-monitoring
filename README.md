370b8f45b291e68220d31956df558e62716bb729
Karol Sendyka <k.sendyka@smartrecruiters.com>
2022-04-24 13:24:25 +0200

    Added draft of upload page, upload endpoint.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pi-eye
 App to recognise and monitor car traffic
+
+Directories:
+* /templates direstory contains all html templates for flask
+
+Rest API
--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
-@app.route("/event", methods = ['POST'])
+@app.route('/upload')
+def display_upload_page():
+    return render_template('sendimage.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+@app.route("/events", methods=['POST'])
 def register_event():
     app.logger.info('A value for debugging1')
-    return { "hello": "world1"}
+    return {"hello": "world1"}
 
 
-@app.route("/event", methods = ['GET'])
+@app.route("/events", methods=['GET'])
 def get_event():
     app.logger.info('A value for debugging2')
-    return { "hello": "world2"}
+    return {"hello": "world2"}
