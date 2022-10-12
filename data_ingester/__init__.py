37c42c3e23832722064bb3cbb8d627e16c4b5ae1
Karol Sendyka <k.sendyka@smartrecruiters.com>
2022-10-12 22:53:24 +0200

    dded initial postgres integration. Postgres run in separate container. data_ingester connects using driver

---
+++ data_ingester/__init__.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+# Connect to your postgres DB
+connection_string = "host='127.0.0.1' dbname='pieyeDB' user='username' password='pass' port=5488"
+conn = psycopg2.connect(connection_string)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM my_data")
+
+# Retrieve query results
+records = cur.fetchall()
--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 # runs project locally
+pip3.9 install -r requirements.txt
+
+docker run --name pi-eye-postgres -p 5488:5432 -e POSTGRES_USER=username -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=pieyeDB -d postgres
+
 flask run
+
+add nwe module for postgres stuff. or maybe keep in one fiile for now??
+installwd binary !!
+pip install psycopg2-binary
+
+eg:
+
