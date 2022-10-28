92967331ae5c629929342ccf5371b3669b9fd0bf
Karol Sendyka <k.sendyka@smartrecruiters.com>
2022-10-28 11:28:38 +0200

    loading files to database moved to ingester

--- data_ingester/__init__.py
+++ data_ingester/__init__.py
@@ -1,21 +1,22 @@
 import psycopg2
+import os.path
+import pi_eye
+import datetime
 
 dbname = 'pieyeDB'
 user = 'username'
 password = 'pass'
-# 'postgresql://postgres@127.0.0.1:5488/test'
 def connect():
     connection_string = f"host='127.0.0.1' dbname='{dbname}' user='{user}' password='{password}' port=5488"
     print(f"connecting {connection_string}")
     return psycopg2.connect(connection_string)
 
-def insert(object_class):
+def insert(object_class, creation_date):
     connection = connect()
     cursor = connection.cursor()
     sql_insert_query = """ INSERT INTO observation (class, observed_on) 
-                           VALUES (%s,now()) """
-    record_to_insert = [object_class]
-    cursor.execute(sql_insert_query, record_to_insert)
+                           VALUES (%s,%s) """
+    cursor.execute(sql_insert_query, (object_class, creation_date))
     connection.commit()
 
 
@@ -29,3 +30,15 @@ def list():
     records = cursor.fetchall()
     print(records)
     return records;
+
+# returns list of files with classifications
+def load_input_files(upload_dir):
+    result = {}
+    list_of_files_to_process = os.listdir(upload_dir)
+    for file in list_of_files_to_process:
+        absolute_path = os.path.abspath(upload_dir + "/" + file)
+        class_of_object = pi_eye.classify(absolute_path)
+        result[file] = class_of_object
+        file_creation_date = os.path.getmtime(absolute_path)
+        insert(class_of_object, datetime.datetime.fromtimestamp(file_creation_date))
+        os.remove(absolute_path)
--- data_ingester/test_ingester.py
+++ data_ingester/test_ingester.py
@@ -1,10 +1,15 @@
+import datetime
 import unittest
 import data_ingester
 import testing.postgresql
+import tempfile
+import shutil
+import os
+
 
 class DataIngesterTests(unittest.TestCase):
+    UPLOAD_DIR = "./test-data/golf/"
 
-    # import testing.postgresql
     def setUp(self):
         self.postgresql = testing.postgresql.Postgresql(port=5488)
         print(self.postgresql.url())
@@ -22,18 +27,31 @@ class DataIngesterTests(unittest.TestCase):
 
     def test_no_results_when_empty_database(self):
         self.assertEqual(data_ingester.list(), [])
+
     def test_that_can_retrieve_after_insert(self):
-        data_ingester.insert("class_abc")
+        creationdate = datetime.datetime(1985, 11, 4, 1, 5)
+        data_ingester.insert("class_abc", creationdate)
 
         result = data_ingester.list()
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0][1], "class_abc" )
+        self.assertEqual(result[0][1], "class_abc")
+        self.assertEqual(result[0][2], creationdate)
+
+    def test_load_dir_and_read_data(self):
+        temporary_dir = tempfile.mkdtemp()
+        destination_file = f"{temporary_dir}/04-20210108112804-01.jpg"
+        shutil.copyfile(f"{self.UPLOAD_DIR}/04-20210108112804-01.jpg", destination_file)
+        file_creation_date = datetime.datetime.fromtimestamp(os.path.getmtime(destination_file))
+
+        data_ingester.load_input_files(temporary_dir)
+        self.assertEqual([(1, 'srebrny golf', file_creation_date)] , data_ingester.list())
 
 
 def read_file(path):
     """ Reads and returns the entire contents of a file """
     with open(path, 'r') as f:
         return f.read()
+
 
 if __name__ == '__main__':
     unittest.main()
