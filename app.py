764ea07175ed0037d061adfd4b05612bab18963e
Karol Sendyka <k.sendyka@smartrecruiters.com>
2022-10-28 11:42:59 +0200

    fix rest endpoint

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from pathlib import Path
-import os
+import data_ingester
 
 # Fails because path is bad ;/
 import pi_eye
+
 app = Flask(__name__)
 UPLOAD_DIR = "uploads/"
 Path(UPLOAD_DIR).mkdir(parents=True, exist_ok=True)
@@ -33,7 +34,13 @@ def register_event():
     app.logger.info('A value for debugging1')
     return {"hello": "world1"}
 
+
 # import os
 @app.route("/load", methods=['GET'])
 def load_input_files():
-    return pi_eye.load_input_files(UPLOAD_DIR)
+    data_ingester.load_input_files(UPLOAD_DIR)
+    loaded_data = data_ingester.list()
+    result = ""
+    for observation in loaded_data:
+        result = result + f"{observation[0]} {str(observation[1])} {str(observation[2])} \n"
+    return result
