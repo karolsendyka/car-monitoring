4ebae5ba204ca796d5918b9964b9ea556c22a15e
Karol Sendykla <karol.sendyka@gmail.com>
2024-10-17 09:45:28 +0200

    new script to prepare data

--- textclassification/bugsToTeamsClassifier/input/convert-csv-to-dirstructure.py
+++ textclassification/bugsToTeamsClassifier/input/convert-csv-to-dirstructure.py
@@ -1,12 +1,35 @@
-# normalize class names
-
-# importing the pandas library
 import pandas as pd
 import os
 from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+# Load the CSV file into a pandas DataFrame
+def split_csv(input_file):
+    # Read the input CSV file
+    data = pd.read_csv(input_file)
+    print("Number of examples per class:")
+    print(data['team'].value_counts())
+
+    # Split the data into 90% training and 10% test
+    train_data, test_data = train_test_split(data, test_size=0.1, random_state=42, shuffle=True)
+
+    # Save the training data to "training.csv"
+    train_data.to_csv('./20241014/training.csv', index=False)
+    print(len(train_data))
+    # Save the test data to "test.csv"
+    test_data.to_csv('./20241014/test.csv', index=False)
+    print(len(test_data))
+
+def removeFile(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
 
 
-def csv_to_dirs(csv_name="bugs_to_teams.csv", data_type="train"):
+
+
+def csv_to_dirs(csv_name="bugsToTeams_20241014.csv", data_type="train"):
     global index, row, f, description, summary, text
     df = pd.read_csv(csv_name, dtype=str)
     for index, row in df.iterrows():
@@ -27,7 +50,12 @@ def csv_to_dirs(csv_name="bugs_to_teams.csv", data_type="train"):
         f.close()
 
 
-# reading the csv file
-csv_to_dirs("training.csv", "train")
-csv_to_dirs("validation.csv", "validation")
+removeFile('./20241014/training.csv')
+removeFile('./20241014/test.csv')
+removeFile('./data/train')
+removeFile('./data/validation')
+split_csv('./20241014/bugsToTeams_20241014.csv')
+
+csv_to_dirs("20241014/training.csv", "train")
+csv_to_dirs("20241014/test.csv", "validation")
 
